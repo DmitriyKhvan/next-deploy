b66dcf32fee92300f1070cfeb5e6dbc13a047b4d
freedom_twin <dimon.twin@gmail.com>
2026-01-18 17:41:20 +0500

    edit error zod

--- src/actions/ingredient.ts
+++ src/actions/ingredient.ts
@@ -33,7 +33,9 @@ export async function createIngredient(formData: FormData) {
     return { success: true, ingredient };
   } catch (error) {
     if (error instanceof ZodError) {
-      return { error: error.errors.map((e) => e.message).join(", ") };
+      return {
+        error: error.issues.map((e) => e.message).join(", "),
+      };
     }
     console.error("Ошибка создания ингредиента:", error);
     return { error: "Ошибка создания ингредиента" };
