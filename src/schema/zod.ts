5558dd57191d5e487af50cf713714b27b70e70bf
freedom_twin <dimon.twin@gmail.com>
2026-01-19 15:19:43 +0500

    edit errors zod

--- src/schema/zod.ts
+++ src/schema/zod.ts
@@ -2,13 +2,21 @@ import { number, object, string } from "zod";
 import { z } from "zod";
 
 export const signInSchema = object({
-  email: string({ required_error: "Email is required" })
+  email: string()
     .min(1, "Email is required")
-    .email("Invalid email"),
-  password: string({ required_error: "Password is required" })
+    .email("Invalid email")
+    .refine((value) => value !== "", {
+      message: "Email is required",
+      path: ["email"],
+    }),
+  password: string()
     .min(1, "Password is required")
     .min(6, "Password must be more than 8 characters")
-    .max(32, "Password must be less than 32 characters"),
+    .max(32, "Password must be less than 32 characters")
+    .refine((value) => value !== "", {
+      message: "Email is required",
+      path: ["email"],
+    }),
 });
 
 export const ingredientSchema = object({
@@ -22,8 +30,12 @@ export const ingredientSchema = object({
     "OTHER",
   ]),
   unit: z.enum(["GRAMS", "KILOGRAMS", "LITERS", "MILLILITERS", "PIECES"]),
-  pricePerUnit: number({ invalid_type_error: "Цена должна быть числом" })
+  pricePerUnit: number()
     .min(0, "Цена должна быть положительной")
-    .nullable(),
+    .nullable()
+    .refine((value) => typeof value === "number", {
+      message: "Цена должна быть числом",
+      path: ["pricePerUnit"],
+    }),
   description: z.string().optional(),
 });
