80db3e4b78cd9e2ccc061c8d6c9c99ba0ed8bd5e
freedom_twin <dimon.twin@gmail.com>
2026-01-20 16:42:44 +0500

    edit prisma utils

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,22 +1,25 @@
 import { PrismaClient } from "@/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaPg } from "@prisma/adapter-pg";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { Pool } from "pg";
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
 };
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+// const adapter = new PrismaPg({
+//   connectionString: process.env.DATABASE_URL,
+// });
 
-const adapter = new PrismaPg(pool);
+// const prisma =
+//   globalForPrisma.prisma ||
+//   new PrismaClient({
+//     adapter,
+//   }).$extends(withAccelerate());
 
 const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    adapter,
+    accelerateUrl: process.env.DATABASE_URL ?? "",
   }).$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
